d6bdb64ffabd57b36633bda0d8324c4b35311459
rj7shakya <shakyarajad1@gmail.com>
2021-02-16 10:18:23 +0545

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-window.onload = () => {
-  'use strict';
+const afterLoading = () => {
+  console.log('loaded');
+  navigator.getInstalledRelatedApps().then((relatedApps) => {
+    console.log('hree');
+    for (let app of relatedApps) {
+      console.log(app.platform);
+      console.log(app.url);
+      console.log(app.id);
+      if (app.id === 'com.novelty.medicaid') {
+        window.location = 'medicaid://';
+      }
+    }
+  });
+  ('use strict');
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js');
   }
 };
+
+window.onload = afterLoading();
