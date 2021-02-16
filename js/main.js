6ddd9826a4e90a3f664d49a97ae8c0a2776770cc
rj7shakya <shakyarajad1@gmail.com>
2021-02-16 16:27:03 +0545

    changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const afterLoading = () => {
-  console.log('loaded');
-  navigator.getInstalledRelatedApps().then((relatedApps) => {
-    console.log('apps', relatedApps);
-    for (let app of relatedApps) {
-      console.log(app.platform);
-      console.log(app.url);
-      console.log(app.id);
-      if (app.id === 'com.novelty.medicaid') {
-        window.location = 'medicaid://host';
-      }
-    }
-  });
   ('use strict');
+  console.log(navigator.getInstalledRelatedApps);
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js');
+    navigator.getInstalledRelatedApps().then((relatedApps) => {
+      console.log('apps', relatedApps);
+      for (let app of relatedApps) {
+        console.log(app.platform);
+        console.log(app.url);
+        console.log(app.id);
+        if (app.id === 'com.novelty.medicaid') {
+          window.location = 'medicaid://host';
+        }
+      }
+    });
   }
 };
 
